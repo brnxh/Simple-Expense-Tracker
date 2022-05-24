5d2018e22da08120deded3733d2a053eccd662c6
brnxh <86363561+brnxh@users.noreply.github.com>
2022-05-24 22:16:34 +0800

    to be continued

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ const formEl = document.querySelector("form");
 const tbodyEl = document.querySelector("tbody");
 const tableEl = document.querySelector("table");
 
+
+//const msg = document.querySelector("msg");
+
+
+
 function addExpense(e)
 {
     e.preventDefault();
+    
     const name = document.getElementById("name").value;
     const date = document.getElementById("dates").value;
     const amount = document.getElementById("amt").value;
@@ -14,19 +20,31 @@ function addExpense(e)
         <td>${date}</td>
         <td>${amount}</td>
         <td><button class = "closeBtn">x</button></td>
+
     </tr>
     `;
 
+    //const msg = document.getElementById('msg').style.display="none";
+
+    
+
 }
+
+
+
 
 function closeRow(e)
 {
     if(!e.target.classList.contains("closeBtn")){
         return;
-    }
+} 
+
 
     const btn = e.target;
     btn.closest("tr").remove();
+
+
+
 }
 
 formEl.addEventListener("submit", addExpense);
